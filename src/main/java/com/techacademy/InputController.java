462766eb69d1936dff4266915e288c74be481ab2
HIsahihi <jiuqingshui47@gmail.com>
2023-12-23 23:24:26 +0900

    lesson15 課題修正

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -1,8 +1,9 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InputController {
 
 	@GetMapping("/input")
-		public String getInput() {
+		public String getInput(@RequestParam(name="previous",required = false) String val, Model model) {
+		model.addAttribute("previous", val);
 			return "input";
 		}
 
